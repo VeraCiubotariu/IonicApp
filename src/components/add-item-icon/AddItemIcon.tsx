26c093fa5a4d2113739ba03d387a6e4a0c5b8f45
VeraCiubotariu <veraciubotariu02@gmail.com>
2024-11-18 13:09:34 +0200

    Ionic3

--- src/components/add-item-icon/AddItemIcon.tsx
+++ src/components/add-item-icon/AddItemIcon.tsx
@@ -1,11 +1,31 @@
-import { IonFab, IonFabButton, IonIcon } from "@ionic/react";
+import { createAnimation, IonFab, IonFabButton, IonIcon } from "@ionic/react";
 import { add } from "ionicons/icons";
 import "./AddItemIcon.css";
+import { useEffect, useRef } from "react";
 
 export const AddItemIcon = ({ history }: { history: any }) => {
+  const fabRef = useRef(null);
+
+  useEffect(() => {
+    if (fabRef.current) {
+      const animation = createAnimation()
+        .addElement(fabRef.current)
+        .duration(2000)
+        .iterations(Infinity)
+        .keyframes([
+          { offset: 0, width: "64px", height: "64px" },
+          { offset: 0.5, width: "56px", height: "56px" },
+          { offset: 1, width: "64px", height: "64px" },
+        ]);
+
+      animation.play();
+    }
+  }, []);
+
   return (
     <IonFab vertical="bottom" horizontal="end" slot="fixed">
       <IonFabButton
+        ref={fabRef}
         onClick={() => {
           history.push("/item");
         }}
